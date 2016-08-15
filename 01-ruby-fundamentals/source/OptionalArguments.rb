a08b698685cedf4b478497d9187f206a62b5b105
Chris McAnally <chris@adadevelopersacademy.org>
2016-08-14 20:46:29 -0700

    Updated Example Source

--- 01-ruby-fundamentals/source/OptionalArguments.rb
+++ 01-ruby-fundamentals/source/OptionalArguments.rb
@@ -1,12 +1,26 @@
-
 # OptionalArguments.rb
 
+# Normal arguments
 def foo(a, b, c)
   puts [a, b]
-
   puts c
 end
 
+# Default Arguments
+def foo2(a, b, c = "Example")
+  puts [a, b]
+  puts c
+end
+
+# Array Arguments
+def sampleArgs(*args)
+  args.each do |arg|
+    puts arg
+  end
+end
+
+
+# Using Array Arguments
 def sampleArgs(a, b, c = 'sample', *d, e)
   print [a, b, c].to_s + "\n"
   puts d.to_s
@@ -14,6 +28,8 @@ def sampleArgs(a, b, c = 'sample', *d, e)
 end
 
 class Book
+
+  # Hash Parameters
   def initialize(args)
     @title = args[:title]
     @author = args[:author]
@@ -27,12 +43,14 @@ class Book
   #    @isbn   = args[:isbn]
   #  end
 
-  def initialize(title, args = {})
-      defaults = {:author => "Ada Lovelace", :isbn => "1234-412"}
-      defaults.merge(args)
-  end
+  # Mixing a Hash Parameter with regular parameters
+  # def initialize(title, args = {})
+  #     defaults = {:author => "Ada Lovelace", :isbn => "1234-412"}
+  #     defaults.merge(args)
+  # end
 end
 
+# Hash Parameters
 def dog(name:  "Fido", breed: "Labrador", weight: 130)
 	puts name + " is a " + breed.to_s + " weighing " + weight.to_s
 end
@@ -40,6 +58,8 @@ end
 dog
 dog(name: 'Spot')
 
+
+# Another hash Parameter
 def foo(bar: 'default')
   puts bar
 end
