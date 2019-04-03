af24d7641e7836075b211200d34dfb664f64dae8
CheezItMan <chris@adadevelopersacademy.org>
2019-04-02 20:18:24 -0700

    update for RW1 controller testing (no fixtures)

--- 08-rails/code_samples/update_controller_test.rb
+++ 08-rails/code_samples/update_controller_test.rb
@@ -1,18 +1,21 @@
 describe "update" do
-  let (:book_hash) {
+  before do
+    Book.create(title: "We're all wonders", author: " R.J. Palacio", description: "Good kids book")
+  end
+  let (:new_book_hash) {
     {
-          book: {
-            title: 'A Wrinkle in Time',
-            author_id: Author.last.id, # valid author_id
-            description: 'A fabulous adventure'
-          }
+      book: {
+        title: "A Wrinkle in Time",
+        author: "Madeleine L'Engle",
+        description: "A fabulous adventure",
+      },
     }
   }
   it "will update a model with a valid post request" do
-    id = books(:poodr).id
+    id = Book.first.id
     expect {
-      patch book_path(id), params: book_hash
-    }.wont_change 'Book.count'
+      patch book_path(id), params: new_book_hash
+    }.wont_change "Book.count"
 
     must_respond_with :redirect
 
@@ -23,12 +26,12 @@ describe "update" do
   end
 
   it "will not update if the params are invalid" do
-    id = books(:poodr).id
-    original_book = books(:poodr)
-    book_hash[:book][:author_id] = -1 # invalid id
+    id = Book.first.id
+    original_book = Book.find_by(id: id)
+
     expect {
-      patch book_path(id), params: book_hash
-    }.wont_change 'Book.count'
+      patch book_path(id), params: {}
+    }.wont_change "Book.count"
 
     must_respond_with :error
 
@@ -42,8 +45,9 @@ describe "update" do
     id = -1
 
     expect {
-      patch book_path(id), params: book_hash
-    }.wont_change 'Book.count'
+      patch book_path(id), params: new_book_hash
+    }.wont_change "Book.count"
 
     must_respond_with :not_found
   end
+end
