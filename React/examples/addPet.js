bd032b662035c6fa0b4ed86341236e00dc0f344a
CheezItMan <chris@adadevelopersacademy.org>
2019-12-03 18:35:02 -0800

    added example post in react

---
+++ React/examples/addPet.js
@@ -0,0 +1,20 @@
+// App.js
+addPet = (pet) => {
+  console.log('pet = ', pet);
+  axios.post('http://localhost:2999/pets', pet)
+    .then((response) => {
+      // We can update the state so we don't need to make another GET request
+      const updatedData = this.state.originalPets;
+      updatedData.push(response.data);
+      this.setState({
+        petList: updatedData,
+        originalPets: updatedData,
+        error: ''
+      });
+    })
+    .catch((error) => {
+      // Use the same idea we had in our GET request
+      this.setState({ error: error.message });
+    });
+}
+;
