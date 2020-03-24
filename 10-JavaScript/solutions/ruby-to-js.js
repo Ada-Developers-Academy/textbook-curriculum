12728c4c26907e58c0415f820058973e6dd6f4aa
beccaelenzil <beccaelenzil@gmail.com>
2020-03-24 14:04:11 -0700

    completed all ruby-to-js solutions

--- 10-JavaScript/solutions/ruby-to-js.js
+++ 10-JavaScript/solutions/ruby-to-js.js
@@ -58,12 +58,38 @@ console.log("bye")
 
 console.log("\n6.")
 
+//for..in
 const fruits = ["banana", "apple", "kiwi"]
-fruits.foreach do |fruit|
-  puts "I love #{fruit}!"
-end
+for(let i in fruits){
+  console.log(`I love ${fruits[i]}!`)
+}
 
+//forEach
+const fruits2 = ["banana", "apple", "kiwi"]
+fruits2.forEach(fruit => console.log(`I love ${fruit}!`))
+  
 console.log("\n7.")
 
+let total0 = 0
+const values = [4, 6, 2, 8, 11]
+
+for(const i in values){
+    total0 = total0 + values[i]
+}
+
+console.log(total0)
+
 console.log("\n8.")
+
+const values2 = [8, 5, 3, 10, 14, 2]
+for(const i in values2){
+  let value = values2[i]
+  if (value == 10){
+    console.log("Special case!")
+  }
+  else{
+    console.log(`Regular values like ${value}`)
+  }
+}
+
 
